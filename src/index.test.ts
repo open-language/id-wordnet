bf872693f862e42e6dce482676c916949d95abb3
Sudhanshu Raheja <sudhanshu@leftshift.io>
2020-05-03 02:56:33 +0700

    [sudhanshu] upgraded version numbers

--- src/index.test.ts
+++
@@ -1,7 +0,0 @@
-import idWordnet from './index'
-
-describe('Test if the package is usable', () => {
-    test('Check path for the versiom', () => {
-        expect(idWordnet.get('1.2')).toContain("id-wordnet/database/1.2")
-    })
-})
