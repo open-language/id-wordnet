8a523f0d963161b9eb7e408a667f6a4eed1d5d4b
Sudhanshu Raheja <sudhanshu@leftshift.io>
2024-04-13 17:34:11 +0700

    added types and upgraded version

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+// src/index.ts
+import * as path from "path";
+var __dirname = "/Users/sudhanshuraheja/code/src/github.com/open-language/id-wordnet/src";
+var idWordnet = new Map;
+idWordnet.set("1.2", path.join(__dirname, "..", "database", "1.2"));
+var src_default = idWordnet;
+export {
+  src_default as default
+};
